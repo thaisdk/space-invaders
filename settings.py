ba9ec75eadd8246cc375c479e53fc7b8dc4bee7d
thaisdk <tkpm@cin.ufpe.br>
2022-02-01 16:01:05 -0300

    Configurações do jogo

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    # Armazena todas as configurações do jogo
+    
+    def __init__(self):
+        # Inicializa as configurações
+        self.screen_width = 720
+        self.screen_height = 480
