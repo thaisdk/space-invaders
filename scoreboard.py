9d0f71f709be19f0439d4e93ccd96445ce6bb00b
thaisdk <tkpm@cin.ufpe.br>
2022-02-07 16:52:35 -0300

    Score

--- scoreboard.py
+++ scoreboard.py
@@ -36,7 +36,7 @@ class Scoreboard():
 
     def prep_high_score(self):
         high_score = int(round(self.stats.high_score, -1))
-        high_score_str = "{:.}".format(high_score)
+        high_score_str = "{:,}".format(high_score)
         self.high_score_image =  self.font.render(high_score_str, True, self.text_color, 
                                                   self.game_settings.screen_background)
         
@@ -47,7 +47,7 @@ class Scoreboard():
     def prep_score(self):
         self.prep_high_score()
         rounded_score = int(round(self.stats.score, -1))
-        score_str = "{:.}".format(rounded_score)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color, 
                                             self.game_settings.screen_background)
         
